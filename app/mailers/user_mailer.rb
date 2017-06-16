39a1f553f79e2a326122f2dfaf81933e9e7a37d7
Erica Dorenkamp <erica.dorenkamp@gmail.com>
2017-06-16 06:39:54 -0400

    Sent email to wrong email address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,6 +12,6 @@ class UserMailer < ApplicationMailer
     @user = user
     @meetups = meetups
     @url = 'https://prankmode.github.io/meetup-client'
-    mail(to: @user.email, subject: 'Your peek reminders')
+    mail(to: @user.reminder_email, subject: 'Your peek reminders')
   end
 end
